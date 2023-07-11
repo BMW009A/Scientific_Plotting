57b3d45b3ef1c0bc91f5b503f3d867507d370e4c
BMW009A <75197427+BMW009A@users.noreply.github.com>
2023-07-11 20:19:41 +0900

    Create Bar_Plotting_with_labels.py

---
+++ Bar_Plotting_with_labels.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rcParams['figure.figsize']=(9.0, 5.0)
+
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
+io = 'C:\\yourdata.xlsx'
+
+plt.rcParams['xtick.direction'] = 'in'   # inner xtick
+plt.rcParams['font.sans-serif'] = ['SimSun']  # display Chinese
+waste_day = 1.1
+
+# data = pd.read_excel()  # read data using pandas
+
+data_1= np.around(np.array(data), decimals=2)
+data_2= np.around(np.array(data), decimals=2)
+
+x = np.arange(len(labels))  # the label locations
+width = 0.9  # the width of the bars
+y_pos = np.linspace(0,26,14)  # define the middle position of two bars
+
+fig, ax = plt.subplots()
+rects1 = ax.barh(y_pos - width/2, # adjust vertical position of  bar 1. (It is recommended not to modify)
+                 data_1, 
+                 width, label='XX', edgecolor="k",color='grey', alpha = 0.6) # using "hatch ='///' " when you want the contrast theme
+rects2 = ax.barh(y_pos + width/2, # adjust vertical position of  bar 2. (It is recommended not to modify)
+                 data_2, 
+                 width, label='XX', color='white', edgecolor="k") # using "hatch ='\\\\\\' " when you want the contrast theme
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xlabel('X', fontsize = 14.5, weight = 'heavy')
+ax.set_ylabel('Y', fontsize = 14.5, weight = 'heavy')
+ax.set_xticks(range(0,int(max(pollution_total_2030)+200),100), # Define the xticks according to the max value of your data
+              fontsize = 14, 
+              family ='Times New Roman', 
+              weight = 'heavy')
+ax.set_yticks(y_pos, labels, fontsize = 10, family ='SimSun')
+
+ax.bar_label(rects1, fontsize = 9.5, family ='Times New Roman') # add "label_type='center' " to center the label
+ax.bar_label(rects2, fontsize = 9.5, family ='Times New Roman')
+
+font1 = {'family' : 'Times New Roman', 'weight' : 'normal', 'size' : 10,}
+ax.legend(prop=font1)
+
+ax.invert_yaxis()  # labels read top-to-bottom
+
+plt.savefig('C:yourpath\\Horz_bar.png', dpi=330,bbox_inches = 'tight')
+plt.show()
